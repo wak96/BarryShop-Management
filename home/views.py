13d1c8b83ad1a0b714bb3b6fe2e7dc2bbca2661e
walidayankawsar <walidayankawsar@gmail.com>
2025-11-19 00:38:06 -0800

    views added

--- home/views.py
+++ home/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home_page(request):
+def home(request):
     return render(request, 'index.html')
+
+def settings(request):
+    return render(request, 'pages/settings.html')
+
+def product(request):
+    return render(request, 'pages/product.html')
+
+def order(request):
+    return render(request, 'pages/orders.html')
+
+def inventory(request):
+    return render(request, 'pages/inventory.html')
+
+def customers(request):
+    return render(request, 'pages/customers.html')
+
+def categories(request):
+    return render(request, 'pages/categories.html')
+
+def barcode(request):
+    return render(request, 'pages/barcode_manager.html')
