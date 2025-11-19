358eb10495ac43914f5c391a0635e3b34b7e090a
walidayankawsar <walidayankawsar@gmail.com>
2025-11-19 00:37:19 -0800

    urls added

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_page, name='home_page')
+    path('', views.home, name='home'),
+    path('settings/', views.settings, name='settings'),
+    path('product/', views.product, name='product'),
+    path('order/', views.order, name='order'),
+    path('inventory/', views.inventory, name='inventory'),
+    path('customers', views.customers, name='customer'),
+    path('categories/', views.categories, name='categories'),
+    path('barcode/', views.barcode, name='barcode'),
 ]
