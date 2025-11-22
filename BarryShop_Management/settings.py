b71983b2230955b807e04fd8f9b428432a87edbc
walidayankawsar <walidayankawsar@gmail.com>
2025-11-22 03:27:38 -0800

    fixed bugs

--- BarryShop_Management/settings.py
+++ BarryShop_Management/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^x#-n2c2yoi$!(2707=#*re$24(b2g@vzw7*tih730m+100m)l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
